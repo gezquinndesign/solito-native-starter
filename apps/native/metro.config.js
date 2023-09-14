9086a6b9e956f0db08a2f9dacd4c682e7d11295a
Gez Quinn <holla@gezquinn.design>
2023-09-14 16:28:01 +0100

    feat: update react-native config files

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,4 +1,10 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+
+// Find the project and workspace directories
+const projectRoot = __dirname;
+// This can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(projectRoot, '../..');
 
 /**
  * Metro configuration
@@ -6,6 +12,17 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    unstable_enableSymlinks: true, // Turn on symlink support
+    nodeModulesPaths: [
+      path.resolve(projectRoot, 'node_modules'),
+      path.resolve(workspaceRoot, 'node_modules'),
+    ],
+    disableHierarchicalLookup: true,
+  },
+  projectRoot,
+  watchFolders: [workspaceRoot],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
